6a9d4b3a886e822ee4faf8881427ab6c9849df8c
Michel Abou Haidar <micchelah98@gmail.com>
2025-09-25 19:01:21 +0300

    cpp03

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabou-ha <mabou-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/25 18:48:36 by mabou-ha          #+#    #+#             */
-/*   Updated: 2025/09/25 18:55:33 by mabou-ha         ###   ########.fr       */
+/*   Created: 2025/09/25 19:00:00 by mabou-ha          #+#    #+#             */
+/*   Updated: 2025/09/25 19:00:01 by mabou-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-
-int main() {
-	ClapTrap a("Alpha");
-	ClapTrap b("Bravo");
-
-	a.attack("Bravo");
-	b.takeDamage(a.getAttackPoints());
-
-	b.beRepaired(5);
-	for (int i = 0; i < 5; ++i)
-		a.attack("a dummy target");
-	b.takeDamage(20);
-	b.beRepaired(3);
-	return 0;
-}
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabou-ha <mabou-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/25 18:48:36 by mabou-ha          #+#    #+#             */
-/*   Updated: 2025/09/25 18:55:33 by mabou-ha         ###   ########.fr       */
+/*   Created: 2025/09/25 19:00:03 by mabou-ha          #+#    #+#             */
+/*   Updated: 2025/09/25 19:00:04 by mabou-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-
-int main() {
-	ClapTrap a("Alpha");
-	ClapTrap b("Bravo");
-
-	a.attack("Bravo");
-	b.takeDamage(a.getAttackPoints());
-
-	b.beRepaired(5);
-	for (int i = 0; i < 5; ++i)
-		a.attack("a dummy target");
-	b.takeDamage(20);
-	b.beRepaired(3);
-	return 0;
-}
